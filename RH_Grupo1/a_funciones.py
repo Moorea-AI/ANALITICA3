dcb4f6592002d83ced0752b70781800cac08577e
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2024-02-25 23:20:10 +0000

    Se toman las funciones del profe y las del semestre pasado para ver cuales pueden servir.

--- RH_Grupo1/a_funciones.py
+++ RH_Grupo1/a_funciones.py
@@ -33,3 +33,92 @@ def ejecutar_sql(nombre_archivo, cur):
     except Exception as e:
         # Imprimir el error si lo hay
         print("Error al ejecutar el archivo SQL:", str(e))
+
+
+#Se podria hacer una función para determinar la matriz de confusion
+#Este lo tomo del trabajo del semestre pasado para ver si funciona.
+
+def show_confusion_matrix(y_true, y_pred):
+    matrix = confusion_matrix(y_true, y_pred)
+    return matrix
+
+#Se hará un imputador? tomare los del profe:
+#REVISAR DE AQUI PARA ABAJO MÁS ADELANTE
+
+def imputar_f (df,list_cat):  
+        
+    
+    df_c=df[list_cat]
+    df_n=df.loc[:,~df.columns.isin(list_cat)]
+
+    imputer_n=SimpleImputer(strategy='median')
+    imputer_c=SimpleImputer(strategy='most_frequent')
+
+    imputer_n.fit(df_n)
+    imputer_c.fit(df_c)
+
+    X_n=imputer_n.transform(df_n)
+    X_c=imputer_c.transform(df_c)
+
+    df_n=pd.DataFrame(X_n,columns=df_n.columns)
+    df_c=pd.DataFrame(X_c,columns=df_c.columns)
+
+    df =pd.concat([df_n,df_c],axis=1)
+    return df
+
+
+def sel_variables(modelos,X,y,threshold):
+    
+    var_names_ac=np.array([])
+    for modelo in modelos:
+        #modelo=modelos[i]
+        modelo.fit(X,y)
+        sel = SelectFromModel(modelo, prefit=True,threshold=threshold)
+        var_names= modelo.feature_names_in_[sel.get_support()]
+        var_names_ac=np.append(var_names_ac, var_names)
+        var_names_ac=np.unique(var_names_ac)
+    
+    return var_names_ac
+
+
+def medir_modelos(modelos,scoring,X,y,cv):
+
+    metric_modelos=pd.DataFrame()
+    for modelo in modelos:
+        scores=cross_val_score(modelo,X,y, scoring=scoring, cv=cv )
+        pdscores=pd.DataFrame(scores)
+        metric_modelos=pd.concat([metric_modelos,pdscores],axis=1)
+    
+    metric_modelos.columns=["reg_lineal","decision_tree","random_forest","gradient_boosting"]
+    return metric_modelos
+
+
+
+def preparar_datos (df):
+   
+    
+
+    #######Cargar y procesar nuevos datos ######
+   
+    
+    #### Cargar modelo y listas 
+    
+   
+    list_cat=joblib.load("list_cat.pkl")
+    list_dummies=joblib.load("list_dummies.pkl")
+    var_names=joblib.load("var_names.pkl")
+    scaler=joblib.load( "scaler.pkl") 
+
+    ####Ejecutar funciones de transformaciones
+    
+    df=imputar_f(df,list_cat)
+    df_dummies=pd.get_dummies(df,columns=list_dummies)
+    df_dummies= df_dummies.loc[:,~df_dummies.columns.isin(['perf_2023','EmpID2'])]
+    X2=scaler.transform(df_dummies)
+    X=pd.DataFrame(X2,columns=df_dummies.columns)
+    X=X[var_names]
+    
+    
+    
+    
+    return X
