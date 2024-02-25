cfcb961d17cc050281fb0ac14653eae0e1d90145
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2024-02-25 22:01:48 +0000

    Importacion de paquetes para crear las funciones

--- RH_Grupo1/a_funciones.py
+++ RH_Grupo1/a_funciones.py
@@ -6,3 +6,11 @@ import matplotlib.pyplot as plt #Para poder ver gráficamente las variables
 import seaborn as sns
 
 from sklearn.metrics import confusion_matrix #Ya que es un problema de clasificación, quiero usar la matriz de confusion para predecir si un empleado se queda o no.
+
+
+#Se toman del archivo del profe por si nos sirven de algo más adelante:
+from sklearn.impute import SimpleImputer 
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import cross_val_predict, cross_val_score, cross_validate
+import joblib
+from sklearn.preprocessing import StandardScaler ## escalar variables 
