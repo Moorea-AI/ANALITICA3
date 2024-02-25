f74ca4af73e571ab39b200e5d91508527b834e9c
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2024-02-25 21:58:56 +0000

    Importacion de paquetes

--- RH_Grupo1/a_funciones.py
+++ RH_Grupo1/a_funciones.py
@@ -1,1 +1,8 @@
 ### Funciones
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt #Para poder ver gráficamente las variables
+import seaborn as sns
+
+from sklearn.metrics import confusion_matrix #Ya que es un problema de clasificación, quiero usar la matriz de confusion para predecir si un empleado se queda o no.
