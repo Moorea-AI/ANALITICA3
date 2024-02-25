e470e609294eedca1b896438d3a8d305fc12a983
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2024-02-25 23:08:15 +0000

    preprocesamiento, inicializacion

---
+++ RH_Grupo1/b_preprocesamiento.py
@@ -0,0 +1,6 @@
+#Se solicita al equipo de analítica proponer estrategias que permitan tomar acciones 
+# para reducir el porcentaje de retiros utilizando la información disponible que se 
+# tiene de los empleados. Asuma que la fecha actual es 1 de enero de 2017, y el plan 
+# y estrategia que proponga, son para reducir los retiros de los empleados durante el 
+# año 2017, para el cuál se espera que el porcentaje de retiros no super el 12% de los 
+# empleados.
