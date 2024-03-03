8a6654de856cbc486dc4c96b6ea34c13329faa60
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2024-03-03 23:20:16 +0000

    Empezamos modelos

--- d_modelos.py
+++ d_modelos.py
@@ -10,10 +10,36 @@
 #                    UNIVERSIDAD DE ANTIOQUIA                  #
 ################################################################
 
+#### Cargar paquetes siempre al inicio
+import sqlite3 as sql #### para bases de datos sql
+import a_funciones as a_funciones ### archivo de funciones propias
+import seaborn as sns
+import numpy as np 
+import graphviz
 
+import pandas as pd ### para manejo de datos
+from pandas.plotting import scatter_matrix  ## para matriz de correlaciones
+
+import matplotlib as mpl ## gráficos
+import matplotlib.pyplot as plt ### gráficos
+
+from sklearn import tree ###para ajustar arboles de decisión
+from sklearn.tree import DecisionTreeClassifier, export_text
+from sklearn.tree import export_text ## para exportar reglas del árbol
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
+
+
+conn = sql.connect("db_empleados") 
+df = pd.read_sql("SELECT * FROM all_employees", conn)
+df.drop('index', axis = 1, inplace = True)  
+#Borramos la columna Index ya que no aporta nada relevante
 
 # Define transformaciones para variables numéricas y categóricas
 numeric_features = ['Age', 'JobSatisfaction', ...]  # Variables numéricas
