7d27fdb08a9b8b75b88c93914e1eedc18ebc1071
Moorea <94485490+Moorea-AI@users.noreply.github.com>
2024-04-30 07:58:04 -0500

    Update b_modelos.py

--- SA_Grupo1/b_modelos.py
+++ SA_Grupo1/b_modelos.py
@@ -12,6 +12,8 @@ from matplotlib import pyplot as plt #
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import roc_auc_score
+from sklearn.linear_model import LogisticRegression
+from keras.utils import to_categorical
 
 
 ### cargar bases_procesadas ####
@@ -44,25 +46,19 @@ np.unique(y_train, return_counts=True)
 np.unique(y_test, return_counts=True)
 
 ##### convertir a 1 d array ############
-
 x_train2 = x_train.reshape(5121, 100*100*3)  # 5121 images in x_train
 x_test2 = x_test.reshape(1279, 100*100*3)  # 1279 images in x_test
 x_train2.shape
 x_test2.shape
 
-# pd.DataFrame(x_train2)
+rl = LogisticRegression()
+rlmodel = rl.fit(x_train2, y_train)
+predrltrain = rlmodel.predict(x_train2)
+print(metrics.classification_report(y_train, predrltrain))
 
+predrltest=rlmodel.predict(x_test2)
+print(metrics.classification_report(y_test, predrltest))
 
-
-############################################################
-###############Seleccionar un indicador ################
-############################################################
-
-### Precision Tasa de acierto en predichos positivos ( qué porcentaje de personas identificada con neumonía realmente tienen)
-### Recall: Tasa de identficación de positivos (de los que tenían neumonía cuantos predije tenían)
-### F1_Socre: Combina Precions y recall (promedio entre dos anteriores)
-### Acurracy: Porcentaje de acertados
-### AUC: detección de positivos vs mala clasificaicón de negativos: porcentaje de los que neumonía que identifico vs los normales que digo que tiene neumonía
 
 ############Analisis problema ###########
 #### me interesa recall: de los enfermos que los pueda detectar, sin embargo
@@ -76,35 +72,15 @@ x_test2.shape
 ############################################################
 ################ Probar modelos de tradicionales#########
 ############################################################
-
-#################### RandomForest ##########
-
 rf=RandomForestClassifier()
 rf.fit(x_train2, y_train)
 
+preddtctrain=rf.predict(x_train2)
+print(metrics.classification_report(y_train, preddtctrain))
 
-pred_train=rf.predict(x_train2)
-print(metrics.classification_report(y_train, pred_train))
-metrics.roc_auc_score(y_train, pred_train, multi_class='ovo')  # Or 'ovr'
+preddtctest=rf.predict(x_test2)
+print(metrics.classification_report(y_test, preddtctest))
 
-pred_test=rf.predict(x_test2)
-print(metrics.classification_report(y_test, pred_test))
-metrics.roc_auc_score(y_test, pred_test, multi_class='ovo')  # Or 'ovr'
-
-# Desempeño de 69% se debe acercar a 1
-
-#################### Decsion tree##########
-
-clf_dt= tree.DecisionTreeClassifier()
-clf=clf_dt.fit(x_train2, y_train)
-
-pred_train=clf_dt.predict(x_train2)
-print(metrics.classification_report(y_train, pred_train))
-metrics.roc_auc_score(y_train, pred_train)
-
-pred_test=clf_dt.predict(x_test2)
-print(metrics.classification_report(y_test, pred_test))
-metrics.roc_auc_score(y_test, pred_test)
 
 
 
@@ -112,13 +88,14 @@ metrics.roc_auc_score(y_test, pred_test)
 ############################################################
 ################ Probar modelos de redes neuronales #########
 ############################################################
-
+y_train1=to_categorical(y_train)
+y_test1=to_categorical(y_test)
 
 fc_model=tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=x_train.shape[1:]), #Se toma x_train original y no el x2, convierte las tres dimensiones en una sola dimensión
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(1, activation='sigmoid')
+    tf.keras.layers.Dense(4, activation='softmax')
 ])
 
 ##### configura el optimizador y la función para optimizar ##############
@@ -127,33 +104,28 @@ fc_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accurac
 
 
 #####Entrenar el modelo usando el optimizador y arquitectura definidas #########
-fc_model.fit(x_train, y_train, batch_size=100, epochs=10, validation_data=(x_test, y_test))
-
+fc_model.fit(x_train, y_train1,  epochs=20, validation_data=(x_test, y_test1))
+#batch_size=100,
 
 #########Evaluar el modelo ####################
-test_loss, test_acc, test_auc, test_recall, test_precision = fc_model.evaluate(x_test, y_test, verbose=2)
-print("Test auc:", test_auc)
-
+test_loss, test_acc, test_auc, test_recall, test_precision = fc_model.evaluate(x_test, y_test1, verbose=2)  
+print("Test recall:", test_recall)
+fc_model.predict(x_test)
 x_test.shape
 
+pred_test=(fc_model.predict(x_test)>0.80).astype("int")
+pred_test.shape
 
-le = LabelEncoder()
-y_test_encoded = le.fit_transform(y_test)  # Encode string labels to numerical labels
-class_names = le.classes_  # Get the ordered list of class names
+pred_test1=np.argmax(pred_test, axis=1)
+y_test2=np.argmax(y_test1, axis=1)
 
-cm = metrics.confusion_matrix(y_test_encoded, pred_test)  # Create confusion matrix
-disp = metrics.ConfusionMatrixDisplay(cm, display_labels=class_names)
+cm = metrics.confusion_matrix(y_test2, pred_test1)  # Create confusion matrix
+disp = metrics.ConfusionMatrixDisplay(cm, display_labels=['NonDemented', 'VeryMildDemented', 'MildDemented', 'ModerateDemented'])
 disp.plot()
 plt.show()
 
 
-###### matriz de confusión test
-# pred_test=(fc_model.predict(x_test) > 0.50).astype('int')
-# cm=metrics.confusion_matrix(y_test,pred_test, labels=[1,0])
-# disp=metrics.ConfusionMatrixDisplay(cm,display_labels=['Pneu', 'Normal'])
-# disp.plot()
-
-print(metrics.classification_report(y_test, pred_test))
+print(metrics.classification_report(y_test1, pred_test))
 
 
 
@@ -161,139 +133,4 @@ print(metrics.classification_report(y_test, pred_test))
 # guardar modelo
 
 fc_model.save('path_to_my_model.h5') 
-
-
-
-######Ejercicio #######
-####usar red neuronal y predecir para las imágenes en la carpeta data\ejercicio_est la probabilidad de tener pneumonia
-
-
-#########################################################################################
-#########################################################################################
-###########Estrategias a usar: regilarization usar una a la vez para ver impacto
-#########################################################################################
-###############################################################
-
-dropout_rate = 0.3 ## porcentaje de neuronas que elimina
-
-fc_model2=tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=x_train.shape[1:]),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-##### configura el optimizador y la función para optimizar ##############
-
-
-fc_model2.compile(optimizer='adam', loss='binary_crossentropy', metrics=['AUC'])
-
-
-#####Entrenar el modelo usando el optimizador y arquitectura definidas #########
-fc_model2.fit(x_train, y_train, batch_size=100, epochs=7, validation_data=(x_test, y_test))
-
-
-####################### aplicar dos regularizaciones L2 y drop out
-###Penaliza el tamaño de los pesos, mientras más grande la penalización menores son los valores de los coeficientes
-
-reg_strength = 0.001
-
-###########Estrategias a usar: regilarization usar una a la vez para ver impacto
-dropout_rate = 0.3 ## porcentaje de neuronas que utiliza 
-
-fc_model3=tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=x_train.shape[1:]),
-    tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-##### configura el optimizador y la función para optimizar ##############
-
-
-
-fc_model3.compile(optimizer='adam', loss='binary_crossentropy', metrics=['AUC'])
-
-#####Entrenar el modelo usando el optimizador y arquitectura definidas #########
-fc_model3.fit(x_train, y_train, batch_size=100, epochs=10, validation_data=(x_test, y_test))
-
-
-
-
-############# Disminuir el numero de neuronas a la mitad
-
-reg_strength = 0.001
-
-###########Estrategias a usar: regilarization usar una a la vez para ver impacto
-dropout_rate = 0.3 ## porcentaje de neuronas que utiliza 
-
-fc_model4=tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=x_train.shape[1:]),
-    tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-##### configura el optimizador y la función para optimizar ##############
-
-
-
-fc_model4.compile(optimizer='adam', loss='binary_crossentropy', metrics=['AUC'])
-
-
-#####Entrenar el modelo usando el optimizador y arquitectura definidas #########
-fc_model4.fit(x_train, y_train, batch_size=100, epochs=10, validation_data=(x_test, y_test))
-
-
-
-reg_strength = 0.001
-
-###########Estrategias a usar: regilarization usar una a la vez para ver impacto
-dropout_rate = 0.2 ## porcentaje de neuronas que utiliza 
-
-fc_model5=tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=x_train.shape[1:]),
-    tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(32, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-##### configura el optimizador y la función para optimizar ##############
-
-
-
-fc_model5.compile(optimizer='adam', loss='binary_crossentropy', metrics=['AUC'])
-
-#####Entrenar el modelo usando el optimizador y arquitectura definidas #########
-fc_model5.fit(x_train, y_train, batch_size=100, epochs=10, validation_data=(x_test, y_test))
-
-
-
-reg_strength = 0.001
-
-###########Estrategias a usar: regilarization usar una a la vez para ver impacto
-dropout_rate = 0.35 ## porcentaje de neuronas que utiliza 
-
-fc_model=tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=x_train.shape[1:]),
-    tf.keras.layers.Dense(128, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(64, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(reg_strength)),
-    tf.keras.layers.Dropout(dropout_rate),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
-##### configura el optimizador y la función para optimizar ##############
-
-
-
-fc_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['AUC'])
-
-#####Entrenar el modelo usando el optimizador y arquitectura definidas #########
-fc_model.fit(x_train, y_train, batch_size=100, epochs=10, validation_data=(x_test, y_test))
 
