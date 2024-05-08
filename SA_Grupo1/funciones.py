7b2198dc6846d52513a742240f1a50db40acf2f2
Moorea-AI <aluz.moreno@udea.edu.co>
2024-05-07 23:04:07 -0500

    correcciones finales

--- SA_Grupo1/funciones.py
+++ SA_Grupo1/funciones.py
@@ -29,3 +29,36 @@ def img2data(path, width=100):
                 labels.append(label_index)
 
     return rawImgs, labels
+
+
+
+
+
+
+
+
+def img2data2(path, width=100):
+    
+    rawImgs = []   #### una lista con el array que representa cada imágen
+    labels = [] ### el label de cada imágen
+    
+    list_labels = [join(path, f) for f in listdir(path)] ### crea una lista de los archivos en la ruta (Normal /Pneumonia)
+
+    for imagePath in list_labels: ### recorre cada carpeta de la ruta ingresada
+        
+        files_list = listdir(imagePath) ### crea una lista con todos los archivos
+        for item in tqdm(files_list):  
+            file = join(imagePath, item) 
+            if file[-1] == 'g':  
+                img = cv2.imread(file)  
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+                img = cv2.resize(img, (width, width))  
+                rawImgs.append(img) 
+                
+                # Clasificar en Alzheimer (1) o No Alzheimer (0)
+                if "Demented" in imagePath:
+                    labels.append(1)
+                else:
+                    labels.append(0)
+
+    return rawImgs, labels
