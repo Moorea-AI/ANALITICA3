1e43c1b6ef54dc3d536dde4620b8adc3c2069d03
Moorea-AI <aluz.moreno@udea.edu.co>
2024-05-07 23:21:31 -0500

    desplieuge

--- SA_Grupo1/funciones.py
+++ SA_Grupo1/funciones.py
@@ -41,24 +41,22 @@ def img2data2(path, width=100):
     
     rawImgs = []   #### una lista con el array que representa cada imágen
     labels = [] ### el label de cada imágen
-    
-    list_labels = [join(path, f) for f in listdir(path)] ### crea una lista de los archivos en la ruta (Normal /Pneumonia)
+    files_list = []
+    list_labels = [path+f for f in listdir(path)] ### crea una lista de los archivos en la ruta
 
-    for imagePath in list_labels: ### recorre cada carpeta de la ruta ingresada
-        
-        files_list = listdir(imagePath) ### crea una lista con todos los archivos
-        for item in tqdm(files_list):  
-            file = join(imagePath, item) 
-            if file[-1] == 'g':  
-                img = cv2.imread(file)  
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
-                img = cv2.resize(img, (width, width))  
-                rawImgs.append(img) 
-                
-                # Clasificar en Alzheimer (1) o No Alzheimer (0)
-                if "Demented" in imagePath:
-                    labels.append(1)
-                else:
-                    labels.append(0)
-
-    return rawImgs, labels
+    for imagePath in (list_labels): ### recorre cada carpeta de la ruta ingresada
+        files_list=listdir(imagePath) ### crea una lista con todos los archivos
+        files_list1 = files_list + files_list1
+        for item in tqdm(files_list): ### le pone contador a la lista: tqdm
+            file = join(imagePath, item) ## crea ruta del archivo
+            if file[-1] =='g': ### verificar que se imágen extensión jpg o jpeg
+                img = cv2.imread(file) ### cargar archivo
+                img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB) ### invierte el orden de los colores en el array para usar el más estándar RGB
+                img = cv2.resize(img ,(width,width)) ### cambia resolución de imágnenes
+                rawImgs.append(img) ###adiciona imágen al array final
+                l = imagePath.split('\\')[2] ### identificar en qué carpeta está
+                if l == 'NonDemented':  ### verificar en qué carpeta está para asignar el label
+                    labels.append([0])
+                elif l == 'Demented':
+                    labels.append([1])
+    return rawImgs, labels, files_list
